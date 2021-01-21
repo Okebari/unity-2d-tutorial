d54e81b065befef70f5325bb74eda1cbe916cf3f
keemeesuu <kimhogy@gmail.com>
2021-01-21 16:26:09 +0900

    prototype upload

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+    CapsuleCollider2D capsuleCollider;
+    public int nextMove;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        
+        Think();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Move
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y );
+
+        // Platform Check
+        Vector2 frontVec = new Vector2(rigid.position.x + nextMove*0.5f, rigid.position.y);
+
+        Debug.DrawRay(frontVec, Vector3.down, new Color(0,1,0));
+        // 레이히트는 관통이 안된다. 한번 맞으면 끝
+        RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+        if(rayHit.collider == null){
+            // Debug.Log("경고");
+            Turn();
+        }
+    }
+    
+    void Think(){
+
+        // Set Next Active
+        nextMove = Random.Range(-1, 2);
+
+        // Sprite Animation
+        anim.SetInteger("WalkSpeed", nextMove);
+
+        // Flip Sprite
+        if(nextMove != 0){
+            spriteRenderer.flipX = nextMove == 1;
+        }
+
+        // Recursive
+        float nextThinkTime = Random.Range(2f, 5f);
+        Invoke("Think", nextThinkTime);
+    }
+
+    void Turn(){
+        // nextMove = nextMove * -1;
+        nextMove *= -1;
+        spriteRenderer.flipX = nextMove == 1;
+        
+        // CancelInvoke("Think");
+        CancelInvoke();
+        Invoke("Think", 5);
+    }
+
+    public void OnDamaged(){
+        // Sprite Alpha
+        spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+
+        // Sprite Flip Y
+        spriteRenderer.flipY = true;
+
+        // Collider Disable
+        capsuleCollider.enabled = false;
+
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+
+        // Destory
+        Invoke("DeActive", 5);
+    }
+
+    void DeActive(){
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// 점수 및 스테이지 관리
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int hp;
+    public PlayerMove player;
+    public GameObject[] Stages;
+
+    // UI
+    public Image[] UIhp;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestartBtn;
+
+    public void NextStage()
+    {
+        // Change Stage
+        if(stageIndex < Stages.Length-1){
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE" + (stageIndex + 1);
+        }else{  // Game Clear
+            // Player Contorl Lock
+            Time.timeScale = 0;
+            // Result UI
+            Debug.Log("Clear!");
+
+            // Restart Button UI
+            UIRestartBtn.SetActive(true);
+            Text btnText = UIRestartBtn.GetComponentInChildren<Text>();
+            btnText.text = "Clear!";
+            UIRestartBtn.SetActive(true);
+        }
+
+        // Calculate Point
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+
+    void Update(){
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+
+    public void HpDown(){
+        if(hp > 1){
+            hp--;
+            UIhp[hp].color = new Color(0, 0, 0, 0.2f);
+        }else{
+            UIhp[0].color = new Color(0, 0, 0, 0.2f);
+
+            // Player Die Effect
+            player.OnDie();
+
+            // Result UI
+            Debug.Log("죽었습니다.");
+
+            // Retry Button UI
+            UIRestartBtn.SetActive(true);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Player"){
+            // Player Reposition
+            if(hp > 1){
+                PlayerReposition();
+            }
+
+            // HP Down
+            HpDown();
+        }
+    }
+
+    void PlayerReposition(){
+        player.transform.position = new Vector3(-7.5f, 0.5f, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart(){
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+}
